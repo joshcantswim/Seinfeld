23748e3daf4c9c4060a2d02fc11493deadfb6582
Josh Shell <joshua.lee.shell@gmail.com>
2023-04-19 17:35:05 -0400

    Add files via upload

--- seinfeld/js/main.js
+++ seinfeld/js/main.js
@@ -1,16 +1,20 @@
-const iceCreamFlavors = ['vanilla', 'chocolate', 'strawberry', 'cookies and cream', 'cookie dough'];
+let Character = [];
+let Dialogue = [];
+let SEID = [];
+let lineCount;
 
- // Append paragraphs and highlight one element
-let p = d3.select('body').selectAll('p')
-    .data(iceCreamFlavors)
-    .enter()
-  .append('p') //now we have a paragraph for each element in the array
-    .text(d => d) //text is set to the element
-    .attr('class', 'custom-paragraph') //a class label allows you to define styles in css, or select these elements later
-    .style('font-weight', 'bold') 
-    .style('color', d => {
-      if(d == 'strawberry')
-        return 'red';
-      else
-        return 'black';
+d3.csv('data/scripts.csv')
+    .then(data => {
+        data.forEach(d => {
+            _data = data;
+            count += 1;
+        })
+
+
+        lineCount = new LineCount({ parentElement: '#linecount' }, data);
+        wordcloud = new Wordcloud({ parentElement: '#wordcloud' }, data);
+        linesperseason = new linesperseason({parentElement: '#linesperseason'}, data);
+
+        lineCount.updateVis();
+        wordcloud.updateVis();
     });
